407ee8779a3465d2cbe299429cc951160e6b2796
Traorasp <miguelsolanopaz@outlook.com>
2022-08-10 08:31:21 -0400

    Connected mongodb and made element schema.

---
+++ Models/Element.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ElementSchema = Schema({
+    Name: {type: String, required: true, maxlength: 50},
+    Weakness: {type: Schema.Types.ObjectId, ref: 'Element'},
+    Strengths: {type: Schema.Types.ObjectId, ref: 'Element'},
+    Description: {type: String, required: true}
+});
+
+ElementSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/element' + this._id;
+});
+
+module.exports = mongoose.model('Element', ElementSchema)
