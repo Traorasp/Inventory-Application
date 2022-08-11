ce2558ac35dfb711d3a5a8309c017da5d8f33e69
Traorasp <miguelsolanopaz@outlook.com>
2022-08-11 08:33:26 -0400

    Added all routes and gremmar fixes in controllers

--- controllers/habitatControllers.js
+++ controllers/habitatControllers.js
@@ -1,33 +1,33 @@
 let Habitat = require('../models/Habitat')
 
-exports.elemnt_list = function(req, res, next) {
+exports.habitat_list = function(req, res, next) {
     res.render('index', { title: "habita list tests" });
 };
 
-exports.elemnt_detail = function(req, res, next) {
+exports.habitat_detail = function(req, res, next) {
     res.render('index', { title: "habita detail tests" });
 };
 
-exports.elemnt_create_get = function(req, res, next) {
+exports.habitat_create_get = function(req, res, next) {
     res.render('index', { title: "habita create tests" });
 };
 
-exports.elemnt_create_post = function(req, res, next) {
+exports.habitat_create_post = function(req, res, next) {
     res.render('index', { title: "habita create post tests" });
 };
 
-exports.elemnt_delete_get = function(req, res, next) {
+exports.habitat_delete_get = function(req, res, next) {
     res.render('index', { title: "habita delete tests" });
 };
 
-exports.elemnt_delete_post = function(req, res, next) {
+exports.habitat_delete_post = function(req, res, next) {
     res.render('index', { title: "habita delete post tests" });
 };
 
-exports.elemnt_update_get = function(req, res, next) {
+exports.habitat_update_get = function(req, res, next) {
     res.render('index', { title: "habita update tests" });
 };
 
-exports.elemnt_update_post = function(req, res, next) {
+exports.habitat_update_post = function(req, res, next) {
     res.render('index', { title: "habita delete post tests" });
 };
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,89 @@
 var express = require('express');
 var router = express.Router();
 
+let elementControllers = require('../controllers/elementControllers')
+let habitatControllers = require('../controllers/habitatControllers')
+let monsterControllers = require('../controllers/monsterControllers')
+let monsterinstanceControllers = require('../controllers/monsterinstanceControllers')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+/// ELEMENT ROUTES ///
+
+
+router.get('/elements', elementControllers.element_list);
+
+router.get('/element/:id', elementControllers.element_detail);
+
+router.get('/element/:id/delete', elementControllers.element_delete_get);
+
+router.post('/element/:id/delete', elementControllers.element_delete_post);
+
+router.get('/element/:id/update', elementControllers.element_update_get);
+
+router.post('/element/:id/update', elementControllers.element_update_post);
+
+router.get('/element/create', elementControllers.element_create_get);
+
+router.post('/element/create', elementControllers.element_create_post);
+
+/// HABITAT ROUTES ///
+
+router.get('/habitats', habitatControllers.habitat_list);
+
+router.get('/habitat/:id', habitatControllers.habitat_detail);
+
+router.get('/habitat/:id/delete', habitatControllers.habitat_delete_get);
+
+router.post('/habitat/:id/delete', habitatControllers.habitat_delete_post);
+
+router.get('/habitat/:id/update', habitatControllers.habitat_update_get);
+
+router.post('/habitat/:id/update', habitatControllers.habitat_update_post);
+
+router.get('/habitat/create', habitatControllers.habitat_create_get);
+
+router.post('/habitat/create', habitatControllers.habitat_create_post);
+
+/// MONSTER ROUTES ///
+
+router.get('/monsters', monsterControllers.monster_list);
+
+router.get('/monster/:id', monsterControllers.monster_detail);
+
+router.get('/monster/:id/delete', monsterControllers.monster_delete_get);
+
+router.post('/monster/:id/delete', monsterControllers.monster_delete_post);
+
+router.get('/monster/:id/update', monsterControllers.monster_update_get);
+
+router.post('/monster/:id/update', monsterControllers.monster_update_post);
+
+router.get('/monster/create', monsterControllers.monster_create_get);
+
+router.post('/monster/create', monsterControllers.monster_create_post);
+
+
+/// MONSTERINSTANCE ROUTES ///
+
+router.get('/monsterinstances', monsterinstanceControllers.monsterinstance_list);
+
+router.get('/monsterinstance/:id', monsterinstanceControllers.monsterinstance_detail);
+
+router.get('/monsterinstance/:id/delete', monsterinstanceControllers.monsterinstance_delete_get);
+
+router.post('/monsterinstance/:id/delete', monsterinstanceControllers.monsterinstance_delete_post);
+
+router.get('/monsterinstance/:id/update', monsterinstanceControllers.monsterinstance_update_get);
+
+router.post('/monsterinstance/:id/update', monsterinstanceControllers.monsterinstance_update_post);
+
+router.get('/monsterinstance/create', monsterinstanceControllers.monsterinstance_create_get);
+
+router.post('/monsterinstance/create', monsterinstanceControllers.monsterinstance_create_post);
 
 module.exports = router;
