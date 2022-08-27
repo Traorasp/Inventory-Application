16c964db8a898a7e4c15a6005e7fe0c0b95e4c02
Traorasp <miguelsolanopaz@outlook.com>
2022-08-26 20:34:30 -0400

    Added security to databse url

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var catalogRouter = require('./routes/catalog');
-
+var dotenv = require('dotenv');
+dotenv.config()
 var app = express();
 app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://Trao:monmon@cluster0.vum70uj.mongodb.net/?retryWrites=true&w=majority';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || process.env.dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
